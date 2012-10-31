ea6976ff3bfd2273eb64d84090bceb73a1e5e4dc
lym737 <lym737@hotmail.com>
2012-10-31 16:07:52 -0700

    Android code commit

---
+++ AndroidApp/src/cmm/view/MediaPage.java
@@ -0,0 +1,40 @@
+package cmm.view;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MediaPage extends Activity{
+	private final String ABOUTUS = "About Us";
+	private final String CONTACTUS = "Contact Us";
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mediapage);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	menu.add(ABOUTUS);
+        menu.add(CONTACTUS);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	Intent intent = new Intent();
+    	if(item.getTitle().equals(ABOUTUS)) {
+    		intent.setClass(this, AboutUs.class);
+    		startActivity(intent);
+    		return true;
+    	} else if (item.getTitle().equals(CONTACTUS)) {
+    		intent.setClass(this, ContactUs.class);
+    		startActivity(intent);
+    		return true;
+    	}
+    	return super.onOptionsItemSelected(item);
+    }
+}
