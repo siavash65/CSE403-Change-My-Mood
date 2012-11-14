0170d862064bd2a7dd052371c30475debce69ddc
hunlan <linhunlan@gmail.com>
2012-11-14 00:42:33 -0800

    Reverted all my changes to client side

--- AndroidApp/src/cmm/view/CMMActivity.java
+++ AndroidApp/src/cmm/view/CMMActivity.java
@@ -4,21 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebView;
 import cmm.model.FacebookHandler;
 
 public class CMMActivity extends Activity{
-	//private WebView ui_background;
-	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        /*ui_background = (WebView) findViewById(R.id.bg_webview);
-        ui_background.loadUrl("file:///android_asset/html/index.html");
-        ui_background.getSettings().setJavaScriptEnabled(true);*/
+
     }
     
 	public void skip_signin(View view){
