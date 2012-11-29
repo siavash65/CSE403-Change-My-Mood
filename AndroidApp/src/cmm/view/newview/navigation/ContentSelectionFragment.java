9b0c6702106a27a138a5cbe3b3a3fae30470afa2
hunlan <linhunlan@gmail.com>
2012-11-29 02:29:58 -0800

    Client - Tabbar added with animation

--- AndroidApp/src/cmm/view/newview/navigation/ContentSelectionFragment.java
+++ AndroidApp/src/cmm/view/newview/navigation/ContentSelectionFragment.java
@@ -3,18 +3,66 @@ package cmm.view.newview.navigation;
 import java.util.Collection;
 import java.util.HashSet;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import cmm.model.Mood;
 import cmm.view.R;
 
 public class ContentSelectionFragment extends Fragment {
+	private static ContentSelectionFragment instance;
+
+	// views
+	private View ui_view;
+	private ViewGroup ui_toplevel;
 	private Collection<Button> ui_contentButtons;
+	private Collection<Button> ui_bars;
+	private Collection<RelativeLayout> ui_buttonLayouts;
+	private Button cur_content;
+
+	// animation
+	private Animation anim_fadeout;
+	private Animation anim_scale;
+
+	private Animation anim_fadein;
+	private Animation anim_scaleback;
+
+	// model
+	private Mood mood;
+	private boolean displayingContent;
+
+	/**
+	 * Get instance method
+	 * 
+	 * @param mood
+	 * @return
+	 */
+	public static ContentSelectionFragment getInstance(Mood mood) {
+		if (instance == null || instance.mood != mood) {
+			instance = new ContentSelectionFragment(mood);
+		}
+		return instance;
+	}
+
+	/**
+	 * private constructor
+	 * 
+	 * @param mood
+	 */
+	private ContentSelectionFragment(Mood mood) {
+		this.mood = mood;
+		this.displayingContent = false;
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,38 +70,125 @@ public class ContentSelectionFragment extends Fragment {
 		View view = inflater.inflate(R.layout.content_fragment, container,
 				false);
 
-		setupComponents(view);
-		handleEvents(view);
-		doLayout(view);
+		this.ui_view = view;
+
+		setupComponents();
+		handleEvents();
+		doLayout();
+
+		// Show content
+		ui_toplevel.setVisibility(View.VISIBLE);
+		doAnimation();
 
 		return view;
 	}
 
 	public void contentClick(View view) {
-		// get an instance of FragmentTransaction from your Fragment
-		FragmentTransaction fragmentTransaction = this.getFragmentManager()
-				.beginTransaction();
+		ui_view.setBackgroundColor(Color.TRANSPARENT);
+		// hide button
+		for (Button b : ui_contentButtons) {
+			if (view == b) {
+				if (b.getId() == R.id.picture_button) {
+					b.setBackgroundResource(R.drawable.content_picture_button_select);
+				} else if (b.getId() == R.id.video_button) {
+					b.setBackgroundResource(R.drawable.content_video_button_select);
+				}
+			}
+			b.startAnimation(anim_fadeout);
+		}
 
-		// remove this fragment
-		fragmentTransaction.remove(this);
-		fragmentTransaction.commit();
+		if (cur_content == null || cur_content != view) {
+			// If no vid/pic set or selected different content
+			if (cur_content != null) {
+				this.cur_content
+						.setBackgroundResource(R.drawable.sign_deselect);
+			}
+
+			// Change the sign color
+			if (view.getId() == R.id.picture_button) {
+				Button pic_button = (Button) ui_view
+						.findViewById(R.id.picture_sign);
+				pic_button.setBackgroundResource(R.drawable.sign_select);
+				this.cur_content = pic_button;
+			} else if (view.getId() == R.id.video_button) {
+				Button vid_button = (Button) ui_view
+						.findViewById(R.id.video_sign);
+				vid_button.setBackgroundResource(R.drawable.sign_select);
+				this.cur_content = vid_button;
+			}
+		}
+
+		Toast.makeText(view.getContext(), "mood = " + mood.value,
+				Toast.LENGTH_SHORT).show();
+	}
+
+	private void afterFadeOutEnd() {
+		for (Button b : ui_contentButtons) {
+			b.setVisibility(View.INVISIBLE);
+		}
+		ui_toplevel.startAnimation(anim_scale);
+	}
+
+	private void afterScaleEnd() {
+		for (Button b : ui_contentButtons) {
+			b.setVisibility(View.GONE);
+		}
+		displayingContent = false;
+	}
+
+	public void reselect() {
+		ui_view.setBackgroundResource(R.drawable.sign_bg_select);
+		for (Button b : ui_contentButtons) {
+			b.setVisibility(View.VISIBLE);
+		}
+		if (!displayingContent) {
+			ui_toplevel.startAnimation(anim_scaleback);
+			displayingContent = true;
+		}
 	}
 
 	/*
 	 * Setup the ui components
 	 */
-	private void setupComponents(View view) {
+	private void setupComponents() {
+		//
+		ui_toplevel = (ViewGroup) ui_view
+				.findViewById(R.id.content_fragment_main);
+
 		// setup content buttons
 		ui_contentButtons = new HashSet<Button>();
-		ui_contentButtons.add((Button) view.findViewById(R.id.picture_button));
-		ui_contentButtons.add((Button) view.findViewById(R.id.video_button));
+		ui_contentButtons.add((Button) ui_view
+				.findViewById(R.id.picture_button));
+		ui_contentButtons.add((Button) ui_view.findViewById(R.id.video_button));
+
+		// setup content bars
+		ui_bars = new HashSet<Button>();
+		ui_bars.add((Button) ui_view.findViewById(R.id.picture_sign));
+		ui_bars.add((Button) ui_view.findViewById(R.id.video_sign));
+
+		// setup layouts
+		ui_buttonLayouts = new HashSet<RelativeLayout>();
+		ui_buttonLayouts.add((RelativeLayout) ui_view
+				.findViewById(R.id.picturebutton_layout));
+		ui_buttonLayouts.add((RelativeLayout) ui_view
+				.findViewById(R.id.videobutton_layout));
+
+		// setup animation
+		anim_fadeout = AnimationUtils.loadAnimation(ui_view.getContext(),
+				R.anim.content_hide);
+		anim_scale = AnimationUtils.loadAnimation(ui_view.getContext(),
+				R.anim.content_layout_scale);
+		anim_fadein = AnimationUtils.loadAnimation(ui_view.getContext(),
+				R.anim.content_show);
+		anim_scaleback = AnimationUtils.loadAnimation(ui_view.getContext(),
+				R.anim.content_layout_scaleback);
 	}
 
 	/*
 	 * Add listeners if necessary
 	 */
-	private void handleEvents(View view) {
-		// TODO
+	private void handleEvents() {
+		// Button click event
 		for (Button b : ui_contentButtons) {
 			b.setOnClickListener(new OnClickListener() {
 
@@ -63,12 +198,51 @@ public class ContentSelectionFragment extends Fragment {
 				}
 			});
 		}
+
+		// Animation call back
+		anim_fadeout.setAnimationListener(new AnimationListener() {
+
+			@Override
+			public void onAnimationStart(Animation animation) {
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+			}
+
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				afterFadeOutEnd();
+			}
+		});
+
+		// Animation call back
+		anim_scale.setAnimationListener(new AnimationListener() {
+
+			@Override
+			public void onAnimationStart(Animation animation) {
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+			}
+
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				afterScaleEnd();
+			}
+		});
 	}
 
 	/*
 	 * Resize layout if necessary
 	 */
-	private void doLayout(View view) {
+	private void doLayout() {
 
+	}
+
+	private void doAnimation() {
+		ui_toplevel.startAnimation(anim_scaleback);
+		displayingContent = true;
 	}
 }
--- AndroidApp/src/cmm/view/newview/navigation/NavigationFragment.java
+++ AndroidApp/src/cmm/view/newview/navigation/NavigationFragment.java
@@ -3,6 +3,7 @@ package cmm.view.newview.navigation;
 import java.util.Collection;
 import java.util.HashSet;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -11,11 +12,14 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
+import cmm.model.Mood;
 import cmm.view.R;
 
 public class NavigationFragment extends Fragment {
+	private View ui_view;
 	private Collection<Button> ui_moodButtons;
+	private Button ui_currentButton;
+	private ContentSelectionFragment csf;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,41 +27,97 @@ public class NavigationFragment extends Fragment {
 		View view = inflater.inflate(R.layout.navigation_fragment, container,
 				false);
 
-		setupComponents(view);
-		handleEvents(view);
-		doLayout(view);
+		this.ui_view = view;
+		
+		setupComponents();
+		handleEvents();
+		doLayout();
 
 		return view;
 	}
 
 	public void moodClick(View view) {
-		// get an instance of FragmentTransaction from your Fragment
-		FragmentTransaction fragmentTransaction = this.getFragmentManager()
-				.beginTransaction();
+		Button clickedButton = (Button) view;
 
-		// add a fragment
-		ContentSelectionFragment csFragment = new ContentSelectionFragment();
-		fragmentTransaction.add(R.id.content_fragment, csFragment);
-		fragmentTransaction.commit();
+		// Determine who clicked
+		int button_id = clickedButton.getId();
+		Mood mood = Mood.HAPPY;
+		switch (button_id) {
+		case R.id.romantic_button:
+			mood = Mood.ROMANTIC;
+			break;
+		case R.id.inspiring_button:
+			mood = Mood.INSPIRED;
+			break;
+		case R.id.enervating_button:
+			mood = Mood.EXCITED;
+			break;
+		}
+
+		
+		boolean differentButton = this.ui_currentButton != clickedButton;
+		if (differentButton) {
+			// Disable Button
+			if (ui_currentButton != null) {
+				ui_currentButton.setBackgroundResource(R.drawable.mood_button);
+				ui_currentButton.setTextColor(Color.DKGRAY);
+			}
+			
+			// Enable Button
+			this.ui_currentButton = clickedButton;
+			ui_currentButton.setBackgroundResource(R.drawable.tab_button_select);
+			ui_currentButton.setTextColor(Color.WHITE);
+		}
+
+		if (this.csf == null) {
+			// get an instance of FragmentTransaction from your Fragment
+			FragmentTransaction fragmentTransaction = this.getFragmentManager()
+					.beginTransaction();
+
+			// add a fragment
+			ContentSelectionFragment csFragment = ContentSelectionFragment
+					.getInstance(mood);
+			fragmentTransaction.add(R.id.content_fragment, csFragment);
+			fragmentTransaction.commit();
+			this.csf = csFragment;
+		} else if (differentButton) {
+			// get an instance of FragmentTransaction from your Fragment
+			FragmentTransaction fragmentTransaction = this.getFragmentManager()
+					.beginTransaction();
+
+			// remove previous
+			fragmentTransaction.remove(this.csf);
+
+			// add a fragment
+			ContentSelectionFragment csFragment = ContentSelectionFragment
+					.getInstance(mood);
+			fragmentTransaction.add(R.id.content_fragment, csFragment);
+			fragmentTransaction.commit();
+			this.csf = csFragment;
+
+		} else {
+			this.csf.reselect();
+		}
+
 	}
 
 	/*
 	 * Setup the ui components
 	 */
-	private void setupComponents(View view) {
+	private void setupComponents() {
 		// setup mood buttons
 		ui_moodButtons = new HashSet<Button>();
-		ui_moodButtons.add((Button) view.findViewById(R.id.funny_button));
-		ui_moodButtons.add((Button) view.findViewById(R.id.romantic_button));
-		ui_moodButtons.add((Button) view.findViewById(R.id.enervating_button));
-		ui_moodButtons.add((Button) view.findViewById(R.id.inspiring_button));
+		ui_moodButtons.add((Button) ui_view.findViewById(R.id.funny_button));
+		ui_moodButtons.add((Button) ui_view.findViewById(R.id.romantic_button));
+		ui_moodButtons.add((Button) ui_view.findViewById(R.id.enervating_button));
+		ui_moodButtons.add((Button) ui_view.findViewById(R.id.inspiring_button));
 
 	}
 
 	/*
 	 * Add listeners if necessary
 	 */
-	private void handleEvents(View view) {
+	private void handleEvents() {
 		// Add listener to mood buttons
 		for (Button b : ui_moodButtons) {
 			b.setOnClickListener(new OnClickListener() {
@@ -72,26 +132,11 @@ public class NavigationFragment extends Fragment {
 	/*
 	 * Resize layout if necessary
 	 */
-	private void doLayout(View view) {
-		
+	private void doLayout() {
+		for (Button b : ui_moodButtons) {
+			b.setTextColor(Color.DKGRAY);
+		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // end
