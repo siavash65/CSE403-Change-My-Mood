ed890e258e4716fc4b74e6e0860179e7cfe98889
Hyunjoon Lee <Pooh@Hyunjoons-MacBook-Pro.local>
2012-12-02 09:13:06 -0800

    to merge

--- AndroidApp/src/cmm/view/newview/CmmActivity.java
+++ AndroidApp/src/cmm/view/newview/CmmActivity.java
@@ -17,8 +17,6 @@ import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
--- AndroidApp/src/cmm/view/newview/FacebookHandler.java
+++ AndroidApp/src/cmm/view/newview/FacebookHandler.java
@@ -27,7 +27,6 @@ public class FacebookHandler extends FacebookActivity{
 			this.loginRedirect();
 		}
 
-		// Hack to prevent double load page from FB's first time load
 		if (firstTime) {
 			firstTime = false;
 		}
@@ -46,8 +45,6 @@ public class FacebookHandler extends FacebookActivity{
 
 	@Override
 	protected void onSessionStateChange(SessionState state, Exception exception) {
-		// this.centerScreen.setImageResource(R.drawable.loading);
-		// user has either logged in or not ...
 		if (state.isOpened()) {
 			// We need an Editor object to make preference changes.
 			// All objects are from android.context.Context
@@ -55,7 +52,6 @@ public class FacebookHandler extends FacebookActivity{
 			SharedPreferences.Editor editor = settings.edit();
 			editor.putBoolean(REDIRECT, true);
 
-			// Commit the edits!
 			editor.commit();
 
 			this.loginRedirect();
@@ -70,97 +66,4 @@ public class FacebookHandler extends FacebookActivity{
 
 		finish();
 	}
-	
-/*	private static FacebookHandler instance = null;
-	private final static String FACEBOOK_APP_ID = "343378635757581";
-	
-	private Facebook FACEBOOK;
-	private boolean usefb;
-	
-	private FacebookHandler(){
-		FACEBOOK = new Facebook(FACEBOOK_APP_ID);
-		usefb = true;
-	}
-	
-	public static FacebookHandler getInstance(){
-		if(instance == null){
-			instance = new FacebookHandler();
-		}
-		return instance;
-	}
-	
-	private void signin_success(Activity activity, Context context){
-		Intent intent = new Intent(context, MoodPage.class);
-		activity.startActivity(intent);
-	}
-	
-	public void doSignin(final Activity activity, final Context context){
-        if(!FACEBOOK.isSessionValid()) {
-        	FACEBOOK.authorize(activity, new String[] {"email"}, new DialogListener(){
-				@Override
-				public void onComplete(Bundle values){
-					signin_success(activity, context);
-					usefb = true;
-				}
-
-				@Override
-				public void onFacebookError(FacebookError error){}
-
-				@Override
-				public void onError(DialogError e){}
-
-				@Override
-				public void onCancel(){}
-            });
-        }else{
-        	signin_success(activity, context);
-        	usefb = true;
-        }
-	}
-	
-	public void doSignout(final Context context, final Menu menu, 
-										final MenuInflater inflater){
-		new Thread(){
-			@Override public void run(){
-				try {
-					FACEBOOK.logout(context);
-				}catch (MalformedURLException e) {
-					e.printStackTrace();
-					temporary_msg();
-				} catch (IOException e) {
-					e.printStackTrace();
-					temporary_msg();
-				} catch (Exception e){
-					e.printStackTrace();
-					temporary_msg();
-				}
-				usefb = false;
-				menu.clear();
-				inflater.inflate(R.menu.menu_basic, menu);
-			}
-		}.start();
-	}
-	
-	// show temporary message for the functions that are not ready yet.
-    private void temporary_msg(){
-    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    	builder.setTitle("Message");
-    	builder.setMessage("Sign out got unexpected error! Please try again.");
-    	builder.setNeutralButton("close", new DialogInterface.OnClickListener() {
-			
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				
-			}
-		});
-    	builder.show();
-    }
-	
-	public void skipFB(){
-		usefb = false;
-	}
-	
-	public boolean getStatus(){
-		return usefb;
-	}*/
 }
