50d3ce1f5eff6c51b4c94743fee062b8b31ac4b9
hunlan <linhunlan@gmail.com>
2012-12-08 16:20:55 -0800

    refine amplification

--- ServerCore-ServerSide/ForceLowDB.py
+++ ServerCore-ServerSide/ForceLowDB.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     medias = Media.objects.all()
     for m in medias:
         try:
-            if m.rank.thumbs_down > 3:
+            if m.rank.thumbs_down > 2:
                 for i in range(0,30):
                     m.thumbs_down()
         except Exception:
